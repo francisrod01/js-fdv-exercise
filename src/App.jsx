0147f96dbcca47fd0815b5faf5b6e72d56a38305
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-12 19:46:42 -0300

    [app] Using bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import 'bootstrap-css-only'; // eslint-disable-line
 import './statics/styles/App.css';
 
 import Header from './components/Header';
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,27 +4,41 @@ import React from 'react';
 const Form = () => (
   <div className="App-intro">
     <form>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+      <div className="form-group row">
+        <label htmlFor="name" className="col-sm-2 col-form-label">Name:</label>
+
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="name" name="name" placeholder="name here..." />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="surname">Surname:</label>
-        <input type="text" id="surname" name="surname" />
+      <div className="form-group row">
+        <label htmlFor="surname" className="col-sm-2 col-form-label">Surname:</label>
+
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="surname" name="surname" placeholder="surname here..." />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="countries">Countries:</label>
-        <select id="countries" name="countries" />
+      <div className="form-group row">
+        <label htmlFor="countries" className="col-sm-2 col-form-label">Countries:</label>
+
+        <div className="col-sm-10">
+          <select className="form-control" id="countries" name="countries">
+            <option selected>Select a country...</option>
+          </select>
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="birthday">Birthday</label>
-        <input type="text" id="birthday" name="birthday" />
+      <div className="form-group row">
+        <label htmlFor="birthday" className="col-sm-2 col-form-label">Birthday</label>
+
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="birthday" name="birthday" placeholder="mm/dd/yyyy" />
+        </div>
       </div>
 
-      <button type="submit">Save</button>
+      <button type="submit" className="btn btn-primary">Save</button>
     </form>
   </div>
 );
