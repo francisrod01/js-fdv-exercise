a1016687a057110ca5959310b45005650e620c71
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-13 17:34:14 -0300

    [app] Added CompleteName component

---
+++ src/components/CompleteName.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const CompleteName = () => (
+  <div className="m-sm-2">
+    <div className="m-sm-2 text-right">
+      {'Your Name and Last Name'}
+    </div>
+  </div>
+);
+
+export default CompleteName;
--- src/components/GridRight.jsx
+++ src/components/GridRight.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import DataGrid from './DataGrid';
+import CompleteName from './CompleteName';
 
 
 const GridRight = () => (
   <div className="flex-column d-flex align-items-stretch">
     <DataGrid />
+    <CompleteName />
   </div>
 );
 
