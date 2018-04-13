24be43446430d470770b2b9007f5545aa37ef3e2
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-13 14:42:31 -0300

    [app] Ajusted inputs alignment in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ const Form = () => (
       <div className="form-group row">
         <label htmlFor="name" className="col-sm-4 col-lg-2 col-form-label">Name:</label>
 
-        <div className="col-sm-10">
+        <div className="col-sm-11 col-lg-9">
           <input type="text" className="form-control border border-primary" id="name" name="name" placeholder="name here..." />
         </div>
       </div>
@@ -15,7 +15,7 @@ const Form = () => (
       <div className="form-group row">
         <label htmlFor="surname" className="col-sm-4 col-lg-2 col-form-label">Surname:</label>
 
-        <div className="col-sm-10">
+        <div className="col-sm-11 col-lg-9">
           <input type="text" className="form-control border border-primary" id="surname" name="surname" placeholder="surname here..." />
         </div>
       </div>
@@ -23,7 +23,7 @@ const Form = () => (
       <div className="form-group row">
         <label htmlFor="countries" className="col-sm-4 col-lg-2 col-form-label">Countries:</label>
 
-        <div className="col-sm-10">
+        <div className="col-sm-11 col-lg-9">
           <select className="form-control border border-primary" id="countries" name="countries">
             <option selected>Select a country...</option>
           </select>
@@ -31,15 +31,15 @@ const Form = () => (
       </div>
 
       <div className="form-group row">
-        <label htmlFor="birthday" className="col-sm-4 col-lg-2 col-form-label">Birthday</label>
+        <label htmlFor="birthday" className="col-sm-4 col-lg-2 col-form-label">Birthday:</label>
 
-        <div className="col-sm-10">
+        <div className="col-sm-11 col-lg-9">
           <input type="text" className="form-control border border-primary" id="birthday" name="birthday" placeholder="mm/dd/yyyy" />
         </div>
       </div>
 
       <div className="form-group row">
-        <div className="col-sm-10 col-lg-12">
+        <div className="col-sm-11 col-lg-11">
           <button type="submit" className="btn btn-default  border border-primary float-right">Save</button>
         </div>
       </div>
