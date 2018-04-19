d70011b104f583c5db868d00a2aac700c06d62ff
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-19 11:23:45 -0300

    [app] ENH: Refactored Grid components and pass props to your children

--- app/src/components/GridLeft.jsx
+++ app/src/components/GridLeft.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Form from './Form';
 import WelcomeMessage from './WelcomeMessage';
 
 
-const GridLeft = () => (
-  <div className="flex-column d-flex align-items-start">
-    <Form />
-    <WelcomeMessage />
+const GridLeft = ({ addItem, loadUsers, user }) => (
+  <div className="flex-column d-flex col-5 align-items-start">
+    <Form
+      addItem={addItem}
+      loadUsers={loadUsers}
+    />
+
+    {
+      !!user &&
+      <WelcomeMessage
+        user={user}
+      />
+    }
   </div>
 );
+
+GridLeft.propTypes = {
+  addItem: PropTypes.func.isRequired,
+  loadUsers: PropTypes.func.isRequired,
+
+  user: PropTypes.instanceOf(Object),
+};
+
+GridLeft.defaultProps = {
+  user: null,
+};
 
 export default GridLeft;
--- app/src/components/GridRight.jsx
+++ app/src/components/GridRight.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import DataGrid from './DataGrid';
 import CompleteName from './CompleteName';
 
 
-const GridRight = () => (
+const GridRight = ({
+  loadUsers, onClickGridItem, user, users,
+}) => (
   <div className="flex-column d-flex align-items-stretch">
-    <DataGrid />
-    <CompleteName />
+    <DataGrid
+      loadUsers={loadUsers}
+      onClickGridItem={onClickGridItem}
+
+      users={users}
+    />
+
+    <CompleteName
+      user={user}
+    />
   </div>
 );
+
+GridRight.propTypes = {
+  loadUsers: PropTypes.func.isRequired,
+  onClickGridItem: PropTypes.func.isRequired,
+
+  user: PropTypes.instanceOf(Object),
+  users: PropTypes.instanceOf(Array),
+};
+
+GridRight.defaultProps = {
+  user: null,
+  users: [],
+};
+
 
 export default GridRight;
